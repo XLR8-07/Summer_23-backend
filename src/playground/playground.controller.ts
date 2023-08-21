89887d044ba654161945eebc2dbd9fd5d7a84b35
XLR8-07 <joyashikur@gmail.com>
2023-08-21 10:03:14 +0600

    simple auth api implemented

--- src/playground/playground.controller.ts
+++ src/playground/playground.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserAuthDto } from './dto/playground.dto';
+import { PlaygroundService } from './playground.service';
 
 @Controller('playground')
-export class PlaygroundController {}
+export class PlaygroundController {
+
+    constructor(private readonly playgroundService: PlaygroundService){}
+    @Get()
+    me(): any
+    {
+        return "ME API CAllED";
+    }
+
+    @Get('shariful')
+    getMeShariful(): string
+    {
+        return "SHARIFUL IS SCARED! HIS ID IS 011181226"
+    }
+
+    @Post('auth')
+    userAuth(@Body() userAuthBody: UserAuthDto): any
+    {
+        if(this.playgroundService.userAuth(userAuthBody))
+        {
+            return "LOGGED IN SUCCESSFULLY"
+        }
+        else
+        {
+            return "CREDENTIAL ERROR"
+        }
+    }
+}
--- src/playground/playground.service.ts
+++ src/playground/playground.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UserAuthDto } from './dto/playground.dto';
 
 @Injectable()
-export class PlaygroundService {}
+export class PlaygroundService {
+    private readonly email: string = "abcd@gmail.com"
+    private readonly password: string = "123456"
+    userAuth(userAuthBody: UserAuthDto)
+    {
+        if(userAuthBody.email == this.email && userAuthBody.password == this.password)
+        {
+            return true
+        }
+        return false
+    }
+}
